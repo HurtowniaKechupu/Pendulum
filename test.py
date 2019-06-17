7e02cd9bf3ab77161dcff7e642cbf9750f517aca
HurtowniaKechupu <>
2019-06-17 21:14:38 +0200

    najlepszy dzis

--- test.py
+++ test.py
@@ -11,4 +11,29 @@ def aprox(number, amount,max):
 
 nowa_pozycja_momentu = aprox(7.84, 5, 8)
 
-print(nowa_pozycja_momentu)
+def oblicz_theta (y,x):
+    if (x>0):
+        th = math.acos(y) - math.pi
+    else:
+        th = math.pi - math.acos(y)
+    th += 0.130899
+    if th >= math.pi:
+        th = th - 2*math.pi
+    return th
+
+def oblicz_thetar (y,x):
+    if (x>0):
+        return math.acos(y) - math.pi
+    else:
+        return math.pi - math.acos(y)
+
+print(oblicz_theta(1,0))
+print(aprox(oblicz_theta(1,0),24,math.pi))
+
+
+
+
+print(oblicz_thetar(1, 0))
+print(aprox(oblicz_thetar(1, 0), 24, math.pi))
+
+print(1+2*3)
